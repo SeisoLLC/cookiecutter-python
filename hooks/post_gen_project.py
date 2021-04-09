b9585b125ffe432691e1d586bcbc041acd1c064c
Jon Zeolla <Jon.Zeolla@SeisoLLC.com>
2021-04-09 10:37:58 -0400

    Fix for CI

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,10 +4,22 @@ Post-project generation hook
 """
 
 import subprocess
+import os
 
 subprocess.run(
     ["git", "init", "--initial-branch=main"], capture_output=True, check=True
 )
+if os.environ.get("GITHUB_ACTIONS") == "true":
+    subprocess.run(
+        ["git", "config", "--global", "user.name", "Seiso Automation"],
+        capture_output=True,
+        check=True,
+    )
+    subprocess.run(
+        ["git", " config", "--global", "user.email", "automation@seisollc.com"],
+        capture_output=True,
+        check=True,
+    )
 subprocess.run(["pipenv", "install", "--dev"], capture_output=True, check=True)
 subprocess.run(["pipenv", "run", "invoke", "reformat"], capture_output=True, check=True)
 subprocess.run(["git", "add", "-A"], capture_output=True, check=True)
