c080f919f21deddf9dc9a2ce67107c105b68ae52
Jon Zeolla <Jon.Zeolla@SeisoLLC.com>
2021-07-09 13:30:07 -0400

    Improve docker build_log logging

--- {{cookiecutter.project_slug}}/tasks.py
+++ {{cookiecutter.project_slug}}/tasks.py
@@ -3,7 +3,6 @@
 Task execution tool & library
 """
 
-import ast
 import os
 {%- if cookiecutter.versioning == 'CalVer' %}
 import re
@@ -49,6 +48,24 @@ def process_container(*, container: docker.models.containers.Container) -> None:
         sys.exit(response["StatusCode"])
     else:
         LOG.info("%s", response["logs"])
+
+
+def log_build_log(*, build_err: docker.errors.BuildError) -> None:
+    """Log the docker build log"""
+    iterator = iter(build_err.build_log)
+    finished = False
+    while not finished:
+        try:
+            item = next(iterator)
+            if "stream" in item:
+                if item["stream"] != "\n":
+                    LOG.error("%s", item["stream"].strip())
+            elif "errorDetail" in item:
+                LOG.error("%s", item["errorDetail"])
+            else:
+                LOG.error("%s", item)
+        except StopIteration:
+            finished = True
 
 
 # Tasks
@@ -121,14 +138,7 @@ def build(_c, debug=False):
             )
         except docker.errors.BuildError as build_err:
             LOG.exception("Failed to build, retrieving and logging the more detailed build error...")
-            iterator = iter(build_err.build_log)
-            finished = False
-            while not finished:
-                try:
-                    item = next(iterator)
-                    LOG.error("%s", ast.literal_eval(item)["stream"])
-                except StopIteration:
-                    finished = True
+            log_build_log(build_err=build_err)
             sys.exit(1)
 
 
