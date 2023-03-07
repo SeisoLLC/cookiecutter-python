bf9c477315c54bfe4691ebceeaba82957ac744d3
seanctech <81828825+seanctech@users.noreply.github.com>
2023-03-06 19:21:20 -0500

    Fix bug in post gen hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -159,6 +159,7 @@ def run_post_gen_hook():
         subprocess.run(["git", "tag", "0.0.0"], capture_output=True, check=True)
         {% elif cookiecutter.versioning == 'CalVer' -%}
         subprocess.run(["git", "tag", "{% now 'local', '%Y.%m.00' %}"], capture_output=True, check=True)
+        {% endif %}
     except subprocess.CalledProcessError as error:
         LOG.error(
             "stdout: %s, stderr: %s",
