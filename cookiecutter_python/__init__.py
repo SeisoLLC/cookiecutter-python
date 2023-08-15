4f596624fc22b965a57882c2b101e074cdec164c
Seiso Automation <automation@seisollc.com>
2023-08-15 15:49:06 +0000

    Bump version: 2023.08.01 → 2023.08.02

--- cookiecutter_python/__init__.py
+++ cookiecutter_python/__init__.py
@@ -4,4 +4,4 @@ cookiecutter_python init
 __maintainer__ = "Seiso"
 __copyright__ = "(c) 2023 Seiso, LLC"
 __project_name__ = "cookiecutter_python"
-__version__ = "2023.08.01"
+__version__ = "2023.08.02"
